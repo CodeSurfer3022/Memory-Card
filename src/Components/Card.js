63e58715a3ddc80e68c1dcb23f7e0b1b4a6ccabc
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-10-09 14:12:29 +0530

    Add corresponding images for pokemons

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ function Card(props) {
     return(
         <div className="card" onClick={() => props.handleClick(props.pokemon.id)}>
             <div className="image">
-                <img src="#" alt="poke" />
+                <img src={props.pokemon.image} alt={props.pokemon.name} />
             </div>
             <div className="main">
                 <p>{props.pokemon.name}</p>
